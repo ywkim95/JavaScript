b9aa5629dba9de94c7fee7bb995352c8eda4d2f5
ywkim95 <rladuddn3924@gmail.com>
2023-08-11 20:58:01 +0900

    first lecture
    js

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,30 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특수기호는 언더스코어(_)와 달러($)를 사용할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다.
+ *      1Name, 2Hello
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ *      var const = 'var';
+ */
+let codeFactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log(codeFactory);
+console.log($ive);
+console.log(_yuJin);
+// let 1name='no';
+// let const = 'attention';
+
+/**
+ * Naming Convention2
+ * 
+ * 1) camelCase -> 대부분의 언어에서 많이 사용
+ * 2) snake_case -> 파이썬같은 언어에서 사용
+ * 3) PascalCase -> C# 마이크로소프트 계열의 언어에서 사용함 
+ */
+// example
+const anYuJin = '안유진';
+console.log(anYuJin);
